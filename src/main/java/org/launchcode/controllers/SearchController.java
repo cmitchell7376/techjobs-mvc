b302d0808ee0c00c35810838dc635f7aa7e4dbf1
cmitchell7376 <cmitchell7376@yahoo.com>
2019-06-10 11:59:19 -0500

    improved SearchController with all search

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,6 +24,13 @@ public class SearchController {
 
     @RequestMapping(value = "results")
     public String results(Model model,@RequestParam String searchType,@RequestParam String searchTerm){
+        if(searchType.equalsIgnoreCase("all")){
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("jobs", jobs);
+            model.addAttribute("columns", ListController.columnChoices);
+            return "search";
+
+        }
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", ListController.columnChoices);
